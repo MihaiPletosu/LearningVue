eeb725161302f96542b9fd3da6b0ad7705d0569c
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-04-21 11:18:55 +0300

    Adding the finished result of exercise 5.

---
+++ Exercise 5/app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    visible: true,
+    names: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21],
+      color: 'red',
+      type: 'some type',
+      anotherArray: ['element 1', 'element 2', 'elem 3']
+    }
+  }
+});
