2bef4c916e9cfecb5465c2778dcc399f552adf0f
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-04-26 15:06:02 +0300

    [Improvements] - Refactoring Attack/Special Attack functions. - Created Player/Monster Attack functions.

--- Monster Slayer tutorial/app.js
+++ Monster Slayer tutorial/app.js
@@ -12,22 +12,28 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function(){
-            this.monsterHealth -= this.calculateDamage(3, 10);
-            if(this.checkWin()){
-                return;
-            }
-
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+            this.playerAttacks(3, 10);
+            this.monsterAttacks();
         },
         specialAttack: function(){
-
+            this.playerAttacks(10, 20);
+            this.monsterAttacks();
         },
         heal: function(){
 
         },
         giveUp: function(){
 
+        },
+        monsterAttacks: function(){
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        playerAttacks: function(min, max){
+            this.monsterHealth -= this.calculateDamage(min, max);
+            if(this.checkWin()){
+                return;
+            }
         },
         calculateDamage: function(min, max){
             return Math.max(Math.floor(Math.random() * max) + 1, min);
