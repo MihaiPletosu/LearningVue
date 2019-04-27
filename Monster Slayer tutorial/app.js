29ac7d24048499b332843f3e74360b01d787e102
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-04-27 12:37:37 +0300

    - Implementing Player Heal method.

--- Monster Slayer tutorial/app.js
+++ Monster Slayer tutorial/app.js
@@ -1,55 +1,61 @@
 new Vue({
     el: '#app',
-    data:{
+    data: {
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false
     },
-    methods:{
-        startGame: function(){
+    methods: {
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function(){
+        attack: function () {
             this.playerAttacks(3, 10);
             this.monsterAttacks();
         },
-        specialAttack: function(){
+        specialAttack: function () {
             this.playerAttacks(10, 20);
             this.monsterAttacks();
         },
-        heal: function(){
+        heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            }
+            else{
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
+        },
+        giveUp: function () {
 
         },
-        giveUp: function(){
-
-        },
-        monsterAttacks: function(){
+        monsterAttacks: function () {
             this.playerHealth -= this.calculateDamage(5, 12);
             this.checkWin();
         },
-        playerAttacks: function(min, max){
+        playerAttacks: function (min, max) {
             this.monsterHealth -= this.calculateDamage(min, max);
-            if(this.checkWin()){
+            if (this.checkWin()) {
                 return;
             }
         },
-        calculateDamage: function(min, max){
+        calculateDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
-        checkWin: function(){
-            if(this.monsterHealth <= 0){
-                if(confirm('You Won! New Game?')){
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You Won! New Game?')) {
                     this.startGame();
-                }else{
+                } else {
                     this.gameIsRunning = false;
                 }
                 return true;
-            }else if(this.playerHealth <= 0){
-                if(confirm('You Lost! New Game?')){
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You Lost! New Game?')) {
                     this.startGame();
-                }else{
+                } else {
                     this.gameIsRunning = false;
                 }
                 return true;
